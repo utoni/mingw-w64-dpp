306d3cfbb999ecac67831c26031da53ec273e9a6
Toni Uhlig <matzeton@googlemail.com>
2024-12-03 15:05:54 +0100

    fixed Mingw-w64 update script
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- update_MINGW.sh
+++ update_MINGW.sh
@@ -3,4 +3,4 @@
 MYDIR="$(dirname ${0})"
 cd "${MYDIR}"
 
-git subtree pull --squash --prefix=mingw-w64-build 'https://github.com/utoni/mingw-w64-build-ng.git' master
+git subtree pull --squash --prefix=mingw-w64-build 'https://github.com/utoni/mingw-w64-build-ng.git' main
