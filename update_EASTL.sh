bd121b5953fecd5c05e3f31b742c0ca6016bd8f8
Toni Uhlig <matzeton@googlemail.com>
2025-06-25 12:32:58 +0200

    Stick EASTL/EABase to certain commit hash
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- update_EASTL.sh
+++ update_EASTL.sh
@@ -3,5 +3,5 @@
 MYDIR="$(dirname ${0})"
 cd "${MYDIR}"
 
-git subtree pull --squash --prefix=EASTL 'https://github.com/electronicarts/EASTL' master
-git subtree pull --squash --prefix=EASTL/test/packages/EABase 'https://github.com/electronicarts/EABase' master
+git subtree pull --squash --prefix=EASTL 'https://github.com/electronicarts/EASTL' e045e9d11fe4c9c7cc0e387ea6410524d4dce0d3
+git subtree pull --squash --prefix=EASTL/test/packages/EABase 'https://github.com/electronicarts/EABase' af4ae274795f4c40829428a2c4058c6f512530f4
